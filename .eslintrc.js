f0aa0a8fe61ca248eceb02b4fa31466cb486861f
yungchingl <yungchingl@student.unimelb.edu.au>
2021-03-16 23:56:04 +0900

    setting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  'rules': {
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error']
+  }
+}
