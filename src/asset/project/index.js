b68d66c692fac04637e8108b9e73bba9f7e1a2bf
Yung-Ching Lin <76548593+joycekyery@users.noreply.github.com>
2023-09-29 20:06:09 +1000

    add project unity game

--- src/asset/project/index.js
+++ src/asset/project/index.js
@@ -1,6 +1,7 @@
 import dontRecipeImage from './dontForgetRecipe.png'
 import onwardImage from './onward.jpg'
 import personalWebsiteImage from './personalWebsite.png'
+import NoTimeToDieImage from './No-Time-To-Die.gif'
 export const projects = [
   {
     title: 'Dont Forget Your Recipe',
@@ -18,6 +19,14 @@ export const projects = [
     tools: 'React JS + Material UI + CSS + MongoDb + NodeJS + Express',
     visit: 'https://www.yungchingl.com/',
     sourceCode: 'https://github.com/joycekyery/joycekyery.github.io',
+  },
+  {
+    title: 'Unity Game - No Time To Die',
+    image: NoTimeToDieImage,
+    description:
+      'No Time To Die is an adventure game in which you play as a castle guard trapped on an island, trying to survive.',
+    tools: 'Unity',
+    sourceCode: 'https://github.com/joycekyery/That-Unity-Game',
   },
   {
     title: 'Onward security - Management Platform',
